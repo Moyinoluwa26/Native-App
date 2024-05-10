dcb20e73082cdd7678f902e8f06ded309cddb19d
Oyejide <victormoyin435@gmail.com>
2024-05-10 03:27:58 +0100

    second commit

---
+++ Ara/index.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 4000;
+const URL = process.env.MONGO_URL;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+mongoose.connect(URL).then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`)
+        /*User.insertMany(users);
+        Post.insertMany(posts);*/
+    })
+}).catch((err) => {
+    return console.log(`${err} did not connect`)
+})
+
+
